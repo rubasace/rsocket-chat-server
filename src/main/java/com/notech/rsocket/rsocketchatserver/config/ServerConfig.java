e9679a942316d9d1afdc5a7110058e01b1585a26
rubenpve <rverdugo@travix.com>
2019-10-24 09:29:23 +0100

    Add zero_copy

--- src/main/java/com/notech/rsocket/rsocketchatserver/config/ServerConfig.java
+++ src/main/java/com/notech/rsocket/rsocketchatserver/config/ServerConfig.java
@@ -2,6 +2,7 @@ package com.notech.rsocket.rsocketchatserver.config;
 
 import com.notech.rsocket.rsocketchatserver.controller.ConnectionController;
 import io.rsocket.RSocketFactory;
+import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.netty.server.CloseableChannel;
 import io.rsocket.transport.netty.server.WebsocketServerTransport;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +18,7 @@ class ServerConfig {
         return RSocketFactory.receive()
 //                             .resume()
 //                             .resumeSessionDuration(Duration.ofMinutes(5))
-//                             .frameDecoder(PayloadDecoder.ZERO_COPY)
+                             .frameDecoder(PayloadDecoder.ZERO_COPY)
                              .acceptor(connectionController)
                              .transport(WebsocketServerTransport.create(RSOCKET_PORT))
                              .start()
