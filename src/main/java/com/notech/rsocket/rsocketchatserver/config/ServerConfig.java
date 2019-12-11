b7a82028928fb82231e459ae3a81decf1603e57d
rubenpve <rverdugo@travix.com>
2019-12-11 10:53:08 +0000

    Remove certificates

--- src/main/java/com/notech/rsocket/rsocketchatserver/config/ServerConfig.java
+++ src/main/java/com/notech/rsocket/rsocketchatserver/config/ServerConfig.java
@@ -1,16 +1,12 @@
 package com.notech.rsocket.rsocketchatserver.config;
 
 import com.notech.rsocket.rsocketchatserver.controller.ConnectionController;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
 import io.rsocket.RSocketFactory;
 import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.netty.server.CloseableChannel;
 import io.rsocket.transport.netty.server.WebsocketServerTransport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.netty.http.server.HttpServer;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
@@ -22,20 +18,10 @@ class ServerConfig {
 
     @Bean
     CloseableChannel closableChannel(final ConnectionController connectionController) throws CertificateException, NoSuchAlgorithmException {
-//        SecureRandom random =SecureRandom.getInstanceStrong();
-//        SelfSignedCertificate certificate = new SelfSignedCertificate("nasvigo.duckdns.org", random, 1024);
-        SelfSignedCertificate certificate = new SelfSignedCertificate();
         return RSocketFactory.receive()
-                             //                             .resume()
-                             //                             .resumeSessionDuration(Duration.ofMinutes(5))
                              .frameDecoder(PayloadDecoder.ZERO_COPY)
                              .acceptor(connectionController)
-                             .transport(WebsocketServerTransport.create(HttpServer.create()
-                                                                                  .port(RSOCKET_PORT)
-                                                                                  .secure(sslContextSpec -> sslContextSpec.sslContext(
-                                                                                          SslContextBuilder.forServer(certificate.certificate(), certificate.privateKey())
-                                                                                                           .trustManager(InsecureTrustManagerFactory.INSTANCE)))))
-
+                             .transport(WebsocketServerTransport.create(RSOCKET_PORT))
                              .start()
                              .block();
     }
